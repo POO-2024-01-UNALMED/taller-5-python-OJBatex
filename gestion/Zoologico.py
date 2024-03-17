0c5cb3d76e16cd93203408167687770cc5605ad0
OJBatex <simondr2006@gmail.com>
2024-03-17 17:50:12 -0500

    Intento 1

---
+++ gestion/Zoologico.py
@@ -0,0 +1,34 @@
+import Zona
+
+class Zoologico:
+    def __init__(self, nombre, ubicacion):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+
+    def cantidadTotalAnimales(self):
+        cantAnimales = 0
+        for i in self._zonas:
+            cantAnimales += self._zonas[i].cantidadAnimales()
+        return cantAnimales
+    
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getUbicacion(self):
+        return self._ubicacion
+    
+    def setUbicacion(self, ubi):
+        self._ubicacion = ubi
+
+    def getZona(self):
+        return self._zonas
+    
+    def setZona(self, zona):
+        self._zonas = zona
---
+++ zooAnimales/Reptil.py
@@ -0,0 +1,50 @@
+import Animal
+
+class Reptil(Animal):
+    _listado = []
+    iguanas = 0
+    serpientes = 0
+    def __init__(self, nombre, edad, habitat, genero, \
+                 colorEscamas, largoCola):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._largoCola = largoCola
+        Reptil._listado.append(self)
+
+    
+    @classmethod
+    def crearIguana(cls, nombre, edad, genero):
+        cls.iguanas += 1
+        return Reptil(nombre, edad, "humedal", genero, \
+                      "verde", 3)
+    
+    @classmethod
+    def crearSerpiente(cls, nombre, edad, genero):
+        cls.serpientes += 1
+        return Reptil(nombre, edad, "jungla", genero, \
+                      "blanco", 1)
+    
+    @classmethod
+    def cantidadReptiles(cls):
+        return len(cls._listado)
+    
+
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+    
+    @classmethod
+    def setListado(cls, listado):
+        cls._listado = listado
+
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setColorEscamas(self, color):
+        self._colorEscamas = color
+
+    def getLargoCola(self):
+        return self._largoCola
+    
+    def setLargoCola(self, largo):
+        self._largoCola = largo
